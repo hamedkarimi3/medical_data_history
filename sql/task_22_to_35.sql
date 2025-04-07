4cb13f9bfa5392952540dff20170820e7c6dfb33
hamedkarimi3 <hmdkrm.rtj@gmail.com>
2025-04-07 14:08:59 +0200

    make a readable format on file and did task 30,31,32

--- sql/task_22_to_35.sql
+++ sql/task_22_to_35.sql
@@ -1,89 +1,131 @@
 -- Task 22: Show the number of admissions per patient
-
-SELECT 
-    patient_id, count(*) as total_admissions
-FROM admissions    
-GROUP BY patient_id;
-
+SELECT
+    patient_id,
+    count(*) as total_admissions
+FROM
+    admissions
+GROUP BY
+    patient_id;
 
 -- Task 23:Show the first name, last name, and admission date of patients who were admitted more than once.
-
-SELECT 
+SELECT
     p.first_name,
     p.last_name,
     a.admission_date
-FROM patients p
-JOIN admissions a ON p.patient_id = a.patient_id
-WHERE a.patient_id IN (
-    SELECT patient_id
-    FROM admissions
-    GROUP BY patient_id
-    HAVING COUNT(*) > 1
-);
-
-
+FROM
+    patients p
+    JOIN admissions a ON p.patient_id = a.patient_id
+WHERE
+    a.patient_id IN (
+        SELECT
+            patient_id
+        FROM
+            admissions
+        GROUP BY
+            patient_id
+        HAVING
+            COUNT(*) > 1
+    );
 
 -- Task 24: Show the first name, last name, and number of admissions for each patient.
-SELECT 
+SELECT
     p.first_name,
     p.last_name,
     COUNT(a.patient_id) AS total_admissions
-FROM patients p
-JOIN admissions a ON p.patient_id = a.patient_id
-GROUP BY p.patient_id;
-
-
+FROM
+    patients p
+    JOIN admissions a ON p.patient_id = a.patient_id
+GROUP BY
+    p.patient_id;
 
 -- Task 25: Show the first name, last name, and last admission date for each patient.
- SELECT 
+SELECT
     p.first_name,
     p.last_name,
     MAX(a.admission_date) AS last_admission_date
-FROM patients p
-JOIN admissions a ON p.patient_id = a.patient_id
-GROUP BY p.patient_id;
-
-
-
+FROM
+    patients p
+    JOIN admissions a ON p.patient_id = a.patient_id
+GROUP BY
+    p.patient_id;
 
 -- Task 26: Show the number of patients admitted in each year
-select 
-    year(admission_date) as admission_year, 
+select
+    year (admission_date) as admission_year,
     count(*) as total_patients
-from admissions
-group by year(admission_date)
-order by admission_year;
-
-
+from
+    admissions
+group by
+    year (admission_date)
+order by
+    admission_year;
 
 -- Task 27: Show the first name, last name, and admission count for patients who were admitted in 2021
-SELECT 
+SELECT
     p.first_name,
     p.last_name,
     COUNT(a.patient_id) AS total_admissions
-FROM patients p
-JOIN admissions a ON p.patient_id = a.patient_id
-WHERE YEAR(a.admission_date) = 2021
-GROUP BY p.patient_id;
-
+FROM
+    patients p
+    JOIN admissions a ON p.patient_id = a.patient_id
+WHERE
+    YEAR (a.admission_date) = 2021
+GROUP BY
+    p.patient_id;
 
 -- Task 28: Show the first name, last name, and the doctor who admitted each patient.
-SELECT 
+SELECT
     p.first_name AS patient_first_name,
     p.last_name AS patient_last_name,
     d.first_name AS doctor_first_name,
     d.last_name AS doctor_last_name
-FROM patients p
-JOIN admissions a ON p.patient_id = a.patient_id
-JOIN doctors d ON d.doctor_id = a.attending_doctor_id;
-
-
+FROM
+    patients p
+    JOIN admissions a ON p.patient_id = a.patient_id
+    JOIN doctors d ON d.doctor_id = a.attending_doctor_id;
 
 -- Task 29: Show the number of admissions handled by each doctor.
-SELECT 
+SELECT
     d.first_name AS doctor_first_name,
     d.last_name AS doctor_last_name,
     COUNT(a.patient_id) AS total_admissions
-FROM doctors d
-JOIN admissions a ON d.doctor_id = a.attending_doctor_id
-GROUP BY d.doctor_id;
+FROM
+    doctors d
+    JOIN admissions a ON d.doctor_id = a.attending_doctor_id
+GROUP BY
+    d.doctor_id;
+
+-- Task 30: Unique patients admitted by each doctor
+SELECT
+    d.first_name AS doctor_first_name,
+    d.last_name AS doctor_last_name,
+    COUNT(DISTINCT a.patient_id) AS unique_patients -- distinct patients means no dublicates in the count and only count unique patients
+FROM
+    doctors d
+    JOIN admissions a ON d.doctor_id = a.attending_doctor_id
+GROUP BY
+    d.doctor_id;
+
+-- Task 31: Show the first name and last name of patients who were admitted by more than one doctor.
+SELECT
+    p.first_name,
+    p.last_name
+FROM
+    patients p
+    JOIN admissions a ON p.patient_id = a.patient_id
+GROUP BY
+    p.patient_id
+HAVING
+    COUNT(DISTINCT a.attending_doctor_id) > 1;
+
+-- Task 32: Show the doctor name and the number of different cities their patients are from.
+select
+    d.first_name,
+    d.lastname,
+    count(distinct p.city) as unique_patients
+from
+    doctors d
+    join admissions a on a.attending_doctor_id = d.doctor_id
+    join patients p on p.patient_id = a.patient_id
+group by
+    d.doctor_id;
